70bd5a0de4d6a094fd6ee3b6bb9356c8449eba92
hongng6036-hub <hongng6036@hotmail.com>
2021-06-23 20:52:52 -0500

    Added some comments and print out headers

--- src/com/tts/Main.java
+++ src/com/tts/Main.java
@@ -4,7 +4,7 @@ import java.lang.Math;
 public class Main {
 
     public static void main(String[] args) {
-
+        System.out.println("==========Basic Calculator =========");
         //Basic Calculator
             Calculator sum = new Calculator();
             Calculator substract = new Calculator();
